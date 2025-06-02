6235a26a871e013fb560a6033895ea66f2279b6e
YairMoon <143459675+YairMoon@users.noreply.github.com>
2025-06-01 22:15:21 -0600

    6to metodo

---
+++ Metodos de ordenamiento/06_Couting Sort.py
@@ -0,0 +1,11 @@
+def counting_sort(arr):
+    if not arr:
+        return []
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+    sorted_arr = []
+    for i, c in enumerate(count):
+        sorted_arr.extend([i] * c)
+    return sorted_arr
